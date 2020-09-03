b45f3b02fd34016e96cdae63e229b27e7b9e4928
Ghosalkar <niteshghosalkar99@gmail.com>
2020-09-04 02:27:26 +0530

    unit test correction

--- test/test_storepick_menu_convertor.py
+++ test/test_storepick_menu_convertor.py
@@ -17,6 +17,8 @@ def test_process():
 
         num_of_levels = int((len(headers)) / 3)
 
+        assert True, process(csv_reader, num_of_levels)
+
 
 def test_process_empty_file():
     with open("data/test_data_empty_file.csv", encoding='utf-8') as csvf:
@@ -27,10 +29,14 @@ def test_process_empty_file():
 
         num_of_levels = int((len(headers)) / 3)
 
+        assert True, process(csv_reader, num_of_levels)
+
 
 def test_validate_column_header():
     with open("data/data.csv", encoding='utf-8') as csvf:
         csv_reader = csv.DictReader(csvf)
+
+        validate_column_header(csv_reader)
 
 
 @raises(Exception)
@@ -38,11 +44,14 @@ def test_validate_column_header_invalid_no_columns():
     with open("data/test_data_invalid_no_of_columns.csv", encoding='utf-8') as csvf:
         csv_reader = csv.DictReader(csvf)
 
+        validate_column_header(csv_reader)
+
 
 @raises(Exception)
 def test_validate_column_header_invalid_column_name():
     with open("data/test_data_invalid_column_name.csv", encoding='utf-8') as csvf:
         csv_reader = csv.DictReader(csvf)
+        validate_column_header(csv_reader)
 
 
 def test_make_json():
@@ -53,6 +62,10 @@ def test_make_json():
         headers.pop(0)
 
         num_of_levels = int((len(headers)) / 3)
+
+        make_json(process(csv_reader, num_of_levels), "../test_nested_data.json")
+
+        assert True, path.exists("../test_nested_data.json")
 
 
 @raises(Exception)
